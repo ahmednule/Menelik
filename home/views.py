ea9eb301546d7f3cb3e201d6041a3758491f8dbe
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-07-31 21:32:14 +0300

    trash collection page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+
+schedule = [
+    {
+        'day': 'Mon',
+        'streets': ['Gallana Rd', 'BrookSide']
+    },
+    {
+        'day': 'Tue',
+        'streets': ['Cotton Av', 'Chaka Rd']
+    },
+    {
+        'day': 'Wed',
+        'streets': ['Dennis Pritt Rd', 'Ngong Road']
+    },
+    {
+        'day': 'Thur',
+        'streets': ['Kikambala Rd', 'Ring Rd']
+    },
+    {
+        'day': 'Fri',
+        'streets': ['Menelik Rd', 'Kirichwa Rd']
+    },
+    {
+        'day': 'Sat',
+        'streets': ['Airwings Kodhek Rd', 'Ndemi Rd']
+    }
+]
+
+
+
+
 def home(request):
     return render(request, 'home/index.html')
 
@@ -7,7 +38,10 @@ def contact(request):
     return render(request, 'home/contact.html')
 
 def collection(request):
-    return render(request, 'home/collection.html')
+    context = {
+        "week_schedule": schedule
+    }
+    return render(request, 'home/collection.html', context)
 
 def recycle(request):
     return render(request, 'home/recycle.html')
