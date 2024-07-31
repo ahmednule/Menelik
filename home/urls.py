e382ee11837cc744868d7291e3502c458f4c0f92
ahmednule <nuleahmed6@gmail.com>
2024-07-31 12:32:20 +0300

    new files

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home_page"),
-     path('', views.contact, name="contact_page"),
+     path('', views.home, name="home_page"),
+     path('contact/', views.contact, name="contact_page"),
+     path('collection/', views.collection, name="collection_resident"),
+     path('recycle/', views.recycle, name="recycle_page"),
+     path('about/', views.about, name='about_page'),
+     path('complain/', views.complain, name='complain_page')
+
 ]
